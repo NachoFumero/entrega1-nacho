9a335e42d7eb180a8018c20278ea3319dccfe569
NachoFumero <79651884+NachoFumero@users.noreply.github.com>
2021-05-02 17:59:27 -0300

    version1

---
+++ js/autos.js
@@ -0,0 +1,9 @@
+
+
+function fn_numero() {
+  let num1 = Math.random() * 100 + 100;
+  let num2 = Math.random() * 100 + 100;
+  numero = num1.toFixed() + " " +  num2.toFixed();
+
+  return "Telefono: 099 " + numero;
+}
